cb783909d3691155d36db8b49593600b4f552c51
muxa1508 <muxa1508@inbox.ru>
2022-09-09 10:14:52 +0300

    MagicBox main code complete

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -1,9 +1,38 @@
+import java.util.Random;
+
 public class MagicBox {
 
 
-    public int []boxSlots;
+    public String[] boxSlots;
 
     public MagicBox(int maxItems) {
-        this.boxSlots = new int[maxItems];
+        this.boxSlots = new String[maxItems];
+    }
+
+    public boolean add(String item) {
+        for (int i = 0; i < boxSlots.length; i++) {
+            if (boxSlots[i] == null) {
+                boxSlots[i] = item;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void pick() throws RuntimeException {
+        int a = 0;
+        for (int i = 0; i < boxSlots.length; i++) {
+            if (boxSlots[i] == null) {
+                a++;
+            }
+        }
+
+        if (a != 0) {
+            throw new RuntimeException("Коробка не полна. Осталось заполнить ячеек: " + a);
+        } else {
+            Random random = new Random();
+            int randomInt = random.nextInt(boxSlots.length);
+            System.out.println(boxSlots[randomInt]);
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+
+        MagicBox box = new MagicBox(3);
+
+        box.add("123");
+//        box.add("456");
+//        box.add("789");
+
+        box.pick();
+
+
         System.out.println("Hello world!");
     }
 }
